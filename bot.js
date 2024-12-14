581464fb12785b3a0b4c24f4804832b4043c90eb
fikertag <149786264+fikertag@users.noreply.github.com>
2024-12-14 16:37:53 +0300

    v1 of countup telegram bot

--- bot.js
+++ bot.js
@@ -84,6 +84,6 @@ bot.onText(/\/help/, (msg) => {
 module.exports = (req, res) => {
   bodyParser.json()(req, res, () => {
     bot.processUpdate(req.body);
-    res.sendStatus(200);
+    res.status(200).send();
   });
 };
